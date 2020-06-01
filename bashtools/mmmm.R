f6b814cf641d82b20658c590286b5200e824f816
joonatansipola <joonatan.sipola@tuni.fi>
2020-06-01 14:35:21 +0300

    gitup push

--- bashtools/mmmm.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-d<-scan("stdin", quiet=TRUE)
-cat(paste0("min ", min(d)), 
-    paste0("max ", max(d)), 
-    paste0("median ", median(d)), 
-    paste0("mean ", mean(d)), 
-    sep="\n")
